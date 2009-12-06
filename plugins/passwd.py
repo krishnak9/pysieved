660cb98b573a52d0563cb5e551cf36ec6c06d4ba
Philippe Levan <levan@epix.net>
2009-12-06 13:19:30 -0500

    Add authentication against shadow database.

--- plugins/passwd.py
+++ plugins/passwd.py
@@ -19,10 +19,32 @@
 ## USA
 
 import __init__
+import crypt
 import pwd
+import spwd
 import os
 
 class PysievedPlugin(__init__.PysievedPlugin):
+    def auth(self, params):
+        try:
+            spwent = spwd.getspnam(params['username'])
+        except KeyError:
+            # This may be caused by insufficient permissions.
+            # Although the result is the same, let's try and be
+            # more informative about the failure case.
+            # Unless the system is misconfigured, there should
+            # be an entry for the current uid. If we can look
+            # it up, then it is not a permissions problem.
+            pwent = pwd.getpwuid(0)
+            try:
+                spwent = spwd.getspnam(pwent[0])
+            except KeyError:
+                raise NotImplementedError('insufficient permissions to authenticate against shadow database')
+            return False
+        sp_pwd = spwent[1]
+        check = crypt.crypt(params['password'], sp_pwd)
+        return check == sp_pwd
+
     def lookup(self, params):
         pwent = pwd.getpwnam(params['username'])
         uid = pwent[2]
@@ -31,3 +53,17 @@ class PysievedPlugin(__init__.PysievedPlugin):
         os.setuid(uid)
         return pwent[5]
 
+
+if __name__ == '__main__':
+    c = __init__.TestConfig(uid = -1, gid = -1)
+    n = PysievedPlugin(None, c)
+
+    # We use the current user for testing, otherwise the set[gu]id()
+    # calls in lookup() will fail.
+    pwent = pwd.getpwuid(os.getuid())
+    print 'Testing with username %s' % pwent[0]
+    try:
+        print n.auth({'username': pwent[0], 'password': 'foobar'})
+    except NotImplementedError, e:
+        print str(e)
+    print n.lookup({'username': pwent[0]})
